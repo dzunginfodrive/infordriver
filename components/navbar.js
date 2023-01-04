e22d8d69d31220a1a2bc2025c381839a5b1644ec
Dung Nguyen <dung.nguyen@mindatlas.com>
2023-01-04 09:45:04 +0700

    deploy v2

--- components/navbar.js
+++ components/navbar.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
@@ -14,82 +14,292 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Avatar from '@mui/material/Avatar';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ArchiveIcon from '@mui/icons-material/Archive';
+import FileCopyIcon from '@mui/icons-material/FileCopy';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 const drawerWidth = 240;
 const navItems = [
+  
+  {name:"Blockchain NFT", slug:"Blockchain-NFT"},
+  { name: 'Full stack development', slug: 'full-stack-development' },
+  // {name: 'Web and mobile app development'},
+  { name: 'Services for start ups', slug: 'services-for-start-ups' },
+  { name: 'Android App Development', slug: 'android-app-development' },
+  { name: 'Ios App Development', slug: 'ios-app-development' },
   {name:"About Us", slug:"about-us"},
-  {name:"Contact Us", slug:"contact-us"}
+  {name:"Contact Us", slug:"contact-us"},
 ];
 
 function DrawerAppBar(props) {
   const { window } = props;
   const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [mobileOpenBox, setMobileOpenBox] = React.useState(false);
+  const [mobileSubOpenBox, setSubMobileOpenBox] = React.useState(false);
 
   const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
+   // alert(!mobileSubOpenBox);
+  // alert('here');
+    if(!mobileSubOpenBox){
+      setMobileOpen(true);
+    }else{
+      setMobileOpen(true);
+    }
   };
+
+  const handleDrawerMobileToggle = () => {
+    setMobileOpen(true);
+  };
+
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = useState();
+
+  const [subMenuOpen, setSubMenuOpen] = useState(false);
+  const [subAnchorEl, setSubAnchorEl] = useState();
+
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [mobileAnchorEl, setMobileAnchorEl] = useState();
+
+  const [subMobileMenuOpen, setSubMobileMenuOpen] = useState(false);
+  const [subMobileAnchorEl, setSubMobileAnchorEl] = useState();
+
+  const recordButtonPosition = (event) => {
+    setAnchorEl(event.currentTarget);
+    setMenuOpen(true);
+  }
+
+  const recordSubButtonPosition = (event) => {
+    setSubAnchorEl(event.currentTarget);
+    setSubMenuOpen(true);
+  }
+  const recordMobileButtonPosition = (event) => {
+   // alert(0);
+    setMobileAnchorEl(event.currentTarget);
+    setSubMobileOpenBox(true);
+    setMobileMenuOpen(true);
+  }
+  const recordSubMobileButtonPosition  = (event) => {
+    setSubMobileAnchorEl(event.currentTarget);
+    setSubMobileMenuOpen(true);
+  }
+
+  let closeMenu = () => {
+      setMenuOpen(false);
+  }
+
+  let closeSubMenu = () => {
+    setSubMenuOpen(false);
+  }
+
+  let closeMobileMenu = () => {
+    setMobileMenuOpen(false);
+  }
+
+  let closeSubMobileMenu = () => {
+    setSubMobileMenuOpen(false);
+  }
+
 
   const drawer = (
     <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
-      <Typography variant="h6" sx={{ my: 2 }}>
-        <Avatar alt="Logo" src="https://c5cea5.n3cdn1.secureserver.net/wp-content/uploads/2020/09/INFORDRIVE-LOGO-FINAL-01-1-1-1-1.png" />
+      <Typography variant='h6' sx={{ my: 2 }}>
+        <Avatar
+          alt='Logo'
+          src='https://c5cea5.n3cdn1.secureserver.net/wp-content/uploads/2020/09/INFORDRIVE-LOGO-FINAL-01-1-1-1-1.png'
+        />
       </Typography>
       <Divider />
       <List>
-        <ListItem disablePadding>
-            <ListItemButton component="a" sx={{ textAlign: 'center' }}>
-              <ListItemText primary={'Home'} />
-            </ListItemButton>
-        </ListItem>
-        {navItems.map((item) => (
-          <ListItem key={item} disablePadding>
-            <ListItemButton component="a" sx={{ textAlign: 'center' }}>
-              <ListItemText primary={item.name} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
+              <ListItem disablePadding>
+                <ListItemButton component='a' to="/">
+                  <ListItemText primary={'Home'} />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton 
+                  onClick={recordMobileButtonPosition}
+                  disableElevation>
+                  Our services <KeyboardArrowDownIcon/>
+                </ListItemButton>
+                <Menu
+                  anchorEl={mobileAnchorEl}
+                  open={mobileMenuOpen}
+                  onClose={closeMobileMenu}>
+                  <List>
+                    <ListItem disablePadding>
+                      <ListItemButton component='a' to="/web-and-mobile-app-development">
+                        <ListItemText primary={'Web and app development '} />
+                      </ListItemButton>
+                      <ArrowForwardIosIcon 
+                        onClick={recordSubMobileButtonPosition}
+                        className="customIcon"
+                      />
+                    </ListItem>
+                    <ListItem disablePadding>
+                      <ListItemButton component='a' to="/services-for-start-ups" >
+                        <ListItemText primary={'Services for start ups'} />
+                      </ListItemButton>
+                    </ListItem>
+                    <ListItem disablePadding>
+                      <ListItemButton component='a' to="/Blockchain-NFT" >
+                        <ListItemText primary={'Blockchain and NFT'} />
+                      </ListItemButton>
+                    </ListItem>
+
+                    <ListItem disablePadding>
+                      <ListItemButton component='a' to="/full-stack-development" >
+                        <ListItemText primary={'Full stack development'} />
+                      </ListItemButton>
+                    </ListItem>
+                  </List>
+                </Menu>
+
+                <Menu
+                  anchorEl={subMobileAnchorEl}
+                  open={subMobileMenuOpen}
+                  onClose={closeSubMobileMenu}>
+                  <List>
+                    <ListItem disablePadding>
+                      <ListItemButton component='a' to="/android-app-development">
+                        <ListItemText primary={'Android app development'} />
+                      </ListItemButton>
+                    </ListItem>
+                    <ListItem disablePadding>
+                      <ListItemButton component='a' to="/ios-app-development" >
+                        <ListItemText primary={'IOS app development'} />
+                      </ListItemButton>
+                    </ListItem>
+                  </List>
+                </Menu>
+                </ListItem>
+                <ListItem disablePadding>
+                  <ListItemButton component='a' to="/about-us">
+                    <ListItemText primary={'About us'} />
+                  </ListItemButton>
+                </ListItem>
+
+                <ListItem disablePadding>
+                  <ListItemButton component='a' to="/contact-us">
+                    <ListItemText primary={'Contact us'} />
+                  </ListItemButton>
+                </ListItem>
+            </List>
     </Box>
   );
 
-  const container = window !== undefined ? () => window().document.body : undefined;
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box sx={{ display: 'flex' }} className="navbarMenu">ne
-      <AppBar component="nav">
+    <Box sx={{ display: 'flex' }} className='navbarMenu'>
+      <AppBar component='nav'>
         <Toolbar>
           <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
+            color='inherit'
+            aria-label='open drawer'
+            edge='start'
+            onClick={handleDrawerMobileToggle}
             sx={{ mr: 2, display: { sm: 'none' } }}
           >
             <MenuIcon />
           </IconButton>
           <Typography
-            variant="h4"
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' }, maxWidth: 445 }}
+            variant='h4'
+            component='div'
+            sx={{
+              flexGrow: 1,
+              display: { xs: 'none', sm: 'block' },
+              maxWidth: 445,
+            }}
           >
-            <img alt="Logo" style={{ width: 80, height: 60 }} src="https://c5cea5.n3cdn1.secureserver.net/wp-content/uploads/2020/09/INFORDRIVE-LOGO-FINAL-01-1-1-1-1.png" />
+            <img
+              alt='Logo'
+              style={{ width: 80, height: 60 }}
+              src='https://c5cea5.n3cdn1.secureserver.net/wp-content/uploads/2020/09/INFORDRIVE-LOGO-FINAL-01-1-1-1-1.png'
+            />
           </Typography>
           <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-            <a href='/' >
-                Home            
-            </a>
-            {navItems.map((item) => (
-              <a href={item.slug} key={item.name}>
-                {item.name}
-              </a>
-            ))}
+            <List>
+              <ListItem disablePadding>
+                <ListItemButton component='a' to="/">
+                  <ListItemText primary={'Home'} />
+                </ListItemButton>
+                <ListItemButton 
+                  onClick={recordButtonPosition}
+                  disableElevation>
+                  Our services <KeyboardArrowDownIcon/>
+                </ListItemButton>
+                <Menu
+                  anchorEl={anchorEl}
+                  open={menuOpen}
+                  onClose={closeMenu}>
+                  <List>
+                    <ListItem disablePadding>
+                      <ListItemButton component='a' to="/web-and-mobile-app-development">
+                        <ListItemText primary={'Web and app development '} />
+                      </ListItemButton>
+                      <ArrowForwardIosIcon 
+                        onClick={recordSubButtonPosition}
+                        className="customIcon"
+                      />
+                    </ListItem>
+                    <ListItem disablePadding>
+                      <ListItemButton component='a' to="/services-for-start-ups" >
+                        <ListItemText primary={'Services for start ups'} />
+                      </ListItemButton>
+                    </ListItem>
+                    <ListItem disablePadding>
+                      <ListItemButton component='a' to="/Blockchain-NFT" >
+                        <ListItemText primary={'Blockchain and NFT'} />
+                      </ListItemButton>
+                    </ListItem>
+
+                    <ListItem disablePadding>
+                      <ListItemButton component='a' to="/full-stack-development" >
+                        <ListItemText primary={'Full stack development'} />
+                      </ListItemButton>
+                    </ListItem>
+                  </List>
+                </Menu>
+
+                <Menu
+                  anchorEl={subAnchorEl}
+                  open={subMenuOpen}
+                  onClose={closeSubMenu}>
+                  <List>
+                    <ListItem disablePadding>
+                      <ListItemButton component='a' to="/android-app-development">
+                        <ListItemText primary={'Android app development'} />
+                      </ListItemButton>
+                    </ListItem>
+                    <ListItem disablePadding>
+                      <ListItemButton component='a' to="/ios-app-development" >
+                        <ListItemText primary={'IOS app development'} />
+                      </ListItemButton>
+                    </ListItem>
+                  </List>
+                </Menu>
+
+
+                <ListItemButton component='a' to="/about-us">
+                  <ListItemText primary={'About us'} />
+                </ListItemButton>
+                <ListItemButton component='a' to="/contact-us">
+                  <ListItemText primary={'Contact us'} />
+                </ListItemButton>
+              </ListItem>
+            </List>
           </Box>
         </Toolbar>
       </AppBar>
-      <Box component="nav">
+      <Box component='nav'>
         <Drawer
           container={container}
-          variant="temporary"
+          variant='temporary'
           open={mobileOpen}
           onClose={handleDrawerToggle}
           ModalProps={{
@@ -97,13 +307,16 @@ function DrawerAppBar(props) {
           }}
           sx={{
             display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            '& .MuiDrawer-paper': {
+              boxSizing: 'border-box',
+              width: drawerWidth,
+            },
           }}
         >
           {drawer}
         </Drawer>
       </Box>
-      <Box component="main" sx={{ p: 3 }}>
+      <Box component='main' sx={{ p: 3 }}>
         <Toolbar />
       </Box>
     </Box>
---
+++ pages/full-stack-development.js
@@ -0,0 +1,315 @@
+import * as React from 'react';
+import Header from '../components/header';
+import Footer from '../components/footer';
+import utilStyles from '../styles/utils.module.css';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+//import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import Avatar from '@mui/material/Avatar';
+import FilterAltIcon from '@mui/icons-material/FilterAlt';
+import Link from '@mui/material/Link';
+import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+
+export default function FullstackDevelopment() {
+  return (
+    <div>
+      <Header />
+      <div className='sliderBox'>
+        <img
+          style={{ width: '100%' }}
+          src='images/pexels-helena-lopes-933964.jpg'
+        />
+        <Box sx={{ flexGrow: 1 }} className='sliderContent'>
+          <Grid container spacing={0}>
+            <p>
+            Infodrive has helped its <br/>
+            clients rationalize business <br/> developments, 
+            abetting <br/> development of specialized <br/> solutions at all stages
+            </p>
+            <Button href="/contact-us" className='readmore' variant='text'>
+              Let's Talk <ArrowRightAltIcon />
+            </Button>
+            <nav className='socical-network'>
+              <List>
+                <ListItem>
+                  <ListItemButton>
+                    <ListItemIcon>
+                      <FacebookIcon />
+                    </ListItemIcon>
+                  </ListItemButton>
+                </ListItem>
+                <ListItem>
+                  <ListItemButton>
+                    <ListItemIcon>
+                      <TwitterIcon />
+                    </ListItemIcon>
+                  </ListItemButton>
+                </ListItem>
+                <ListItem>
+                  <ListItemButton>
+                    <ListItemIcon>
+                      <InstagramIcon />
+                    </ListItemIcon>
+                  </ListItemButton>
+                </ListItem>
+              </List>
+            </nav>
+          </Grid>
+        </Box>
+      </div>
+
+      <section className={`sectionBox`}>
+        <Typography gutterBottom variant='h3' component='div'>
+          APPLICATION DEVELOPMENT, <br/>MAINTENANCE AND SUPPORT
+        </Typography>
+        <Typography gutterBottom variant='h2' component='div'>
+          Knowledge combined with quality gives our clients the edge over other competitors.
+          Value assurance practices and optimized delivery cycles has been our focus from day one.
+        </Typography>
+        <Typography gutterBottom variant='h5' component='div'>
+          Our niche solutions are one of a kind in the market. Our applications maximize on key factors such as portability and scalability.  Infodrive enables clients to rationalize business developments and abet development of specialized solutions at all stages. We provide you highly differentiated and nifty solutions. Our applications provide maximum portability and scalability
+        </Typography>
+      </section>
+
+      <section className={`sectionBox whyUsBox pb0 fullstackGame`}>
+        <Typography gutterBottom variant='h2' component='div'>
+        Stay ahead in the game with our full stack
+        </Typography>
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={0}>
+
+            <Grid item xs={4}>
+              <Card sx={{ maxWidth: 345 }}>
+                <Avatar
+                  alt='Remy Sharp'
+                  src='images/vector2.png'
+                  sx={{ width: 65, height: 65, marginLeft: 2 }}
+                />
+                <CardContent>
+                  <Typography
+                    gutterBottom
+                    variant='h3'
+                    className='pt15 pb15'
+                    component='div'
+                  >
+                    Visual<br/>engineering
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+
+            <Grid item xs={4}>
+              <Card sx={{ maxWidth: 345 }}>
+                <Avatar
+                  alt='Remy Sharp'
+                  src='images/vector3.png'
+                  sx={{ width: 65, height: 65, marginLeft: 2 }}
+                />
+                <CardContent>
+                  <Typography
+                    gutterBottom
+                    variant='h3'
+                    className='pt15 pb15'
+                    component='div'
+                  >
+                    Product <br/> development
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+
+            <Grid item xs={4}>
+              <Card sx={{ maxWidth: 345 }}>
+                <Avatar
+                  alt='Remy Sharp'
+                  src='images/vector1.png'
+                  sx={{ width: 65, height: 65, marginLeft: 2 }}
+                />
+                <CardContent>
+                  <Typography
+                    gutterBottom
+                    variant='h3'
+                    className='pt15 pb15'
+                    component='div'
+                  >
+                    Application<br/>development
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+
+            <Grid item xs={4}>
+              <Card sx={{ maxWidth: 345 }}>
+                  <Avatar
+                    alt='Remy Sharp'
+                    src='images/vector4.png'
+                    sx={{ width: 65, height: 65, marginLeft: 2 }}
+                  />
+                  <CardContent>
+                    <Typography
+                      gutterBottom
+                      variant='h3'
+                      className='pt15 pb15'
+                      component='div'
+                    >
+                      Porting <br/> migration
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+
+              <Grid item xs={4}>
+              <Card sx={{ maxWidth: 345 }}>
+                  <Avatar
+                    alt='Remy Sharp'
+                    src='images/vector6.png'
+                    sx={{ width: 65, height: 65, marginLeft: 2 }}
+                  />
+                  <CardContent>
+                    <Typography
+                      gutterBottom
+                      variant='h3'
+                      className='pt15 pb15'
+                      component='div'
+                    >
+                      Production <br/> support
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+
+              <Grid item xs={4}>
+                <Card sx={{ maxWidth: 345 }}>
+                  <Avatar
+                    alt='Remy Sharp'
+                    src='images/vector7.png'
+                    sx={{ width: 65, height: 65, marginLeft: 2 }}
+                  />
+                  <CardContent>
+                    <Typography
+                      gutterBottom
+                      variant='h3'
+                      className='pt15 pb15'
+                      component='div'
+                    >
+                      Product help desk <br/> & support
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+          </Grid>
+        </Box>
+      </section>
+
+      <section className={`sectionBox whyUsBox pb0`}>
+        <Typography gutterBottom variant='h2' className='mb0' component='div'>
+          We ARE <br/>SPECIALIZED IN
+        </Typography>
+        <br />
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={0}>
+            <Grid item xs={4}>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardContent>
+                  <Typography gutterBottom variant='h5' className="pb10" component='div'>
+                  WORLD’S #1 CRM
+                  </Typography>
+                  <Typography gutterBottom variant='h3' component='div'>
+                  Salesforce partner
+                  </Typography>
+                  <br />
+                  <Typography variant='body2' color='text.secondary'>
+                  Salesforce is the World’s #1 CRM today, over 100,000 of the world’s most innovative companies use Salesforce to close bigger deals, faster.
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+
+            <Grid item xs={4}>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardContent>
+                  <Typography gutterBottom variant='h5' className="pb10" component='div'>
+                  AI AND ANALYTICS
+                  </Typography>
+                  <Typography gutterBottom variant='h3' component='div'>
+                  Servicenow
+                  </Typography>
+                  <br />
+                  <Typography variant='body2' color='text.secondary'>
+                  Unleash the power of AI and analytics to surface information, make predictions, and automate repetitive tasks so you can focus on strategic work.
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+
+            <Grid item xs={4}>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardContent>
+                  <Typography gutterBottom variant='h5' className="pb10" component='div'>
+                  ENTERPRISE APPLICATIONS
+                  </Typography>
+                  <Typography gutterBottom variant='h3' component='div'>
+                  Mulesoft
+                  </Typography>
+                  <br />
+                  <Typography variant='body2' color='text.secondary'>
+                  Today’s enterprise needs a network of applications, data, and devices connected by APIs, whether on-premises or in the cloud. 
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+
+          </Grid>
+        </Box>
+      </section>
+
+      <section className={`sectionBox ourValueBox ourBeliefBox`}>
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={0}>
+            <Grid item xs={5} style={{ padding: '30px' }}>
+              <Typography gutterBottom variant='h5' component='div'>
+                Get in touch with us today for a <br /> non-obligation
+                discussion and start transforming your start-up ideas into
+                real-life products.{' '}
+              </Typography>
+              <br />
+              <br />
+              {/* <Button className='readmore pl0' variant='text'>
+               
+              </Button> */}
+              <ListItemButton className="pl0" component="a" sx={{ textAlign: 'center' }}>
+              CONTACT US <ArrowRightAltIcon />
+            </ListItemButton>
+            </Grid>
+            <Grid item xs={7}>
+              <Card>
+                <img
+                  alt='Remy Sharp'
+                  src='images/pexels-prakhar-bansal-732183.jpg'
+                />
+              </Card>
+            </Grid>
+          </Grid>
+        </Box>
+      </section>
+      <Footer />
+    </div>
+  );
+}
